98a70a1de11bf6d0ca36b6e17c06e6a01be90b43
Andrew Crossan <andrewcrossanit@gmail.com>
2021-09-28 16:34:21 +0100

    initial commit

---
+++ console.py
@@ -0,0 +1,15 @@
+import pdb 
+# from models.task import Task
+# from models.user import User
+# import repositories.task_repository as task_repository 
+# import repositories.user_repository as user_repository
+
+
+
+
+
+
+
+
+pdb.set_trace()
+
---
+++ models/album.py
@@ -0,0 +1,5 @@
+class Album:
+    def __init__(self, album_name, genre, id=None):
+        self.album_name = album_name
+        self.genre = genre
+        self.id = id
